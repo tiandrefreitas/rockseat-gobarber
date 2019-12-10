451dcd8816298b6e9d92a993e7507c4002f024b4
Luis Freitas Da Silva <aluisf@indracompany.com>
2019-12-10 10:44:29 -0300

    Autenticação ok

--- frontend-gobarber/src/pages/SignIn/index.js
+++ frontend-gobarber/src/pages/SignIn/index.js
@@ -1,28 +1,41 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
 const schema = Yup.object().shape({
   email: Yup.string()
-    .email('Insira um email valido')
-    .required('O email é obrigatorio'),
-  password: Yup.string().required('A Senha é Obrigatoria'),
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
 });
 
 export default function SignIn() {
-  function handleSubmit() {}
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
+
       <Form schema={schema} onSubmit={handleSubmit}>
-        <Input name="email" type="email" placeholder="Seu Email" />
-        <Input name="password" type="password" placeholder="Sua senha" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
 
         <button type="submit">Acessar</button>
-        <Link to="register">Criar conta gratuita</Link>
+        <Link to="/register">Criar conta gratuita</Link>
       </Form>
     </>
   );
